3e7acc7aa0bef564807d1f5a9590a895084165cf
tatang <lintangpsa@gmail.com>
2025-06-30 19:34:41 +0700

    add closing whe click nav on mobile screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,13 @@ import Navlink from "./Navlink";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 import { SheetContent, SheetTrigger, Sheet, SheetTitle } from "./ui/sheet";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClose = () => setIsOpen(false)
+
   return (
     <header className="py-8 md:py-8 ">
       <div className="container flex justify-between mx-auto">
@@ -23,7 +28,7 @@ const Header = () => {
           <Navlink href="/contact">Hire me</Navlink>
         </nav>
         <nav className="block mr-4 md:hidden">
-          <Sheet>
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger>
               <CiMenuFries size={30} />
             </SheetTrigger>
@@ -33,12 +38,12 @@ const Header = () => {
                   Lintang <span className="text-accent -ml-5">.</span>
                 </SheetTitle>
               </Link>
-              <Navlink href="/">Home</Navlink>
-              <Navlink href="/project">Project</Navlink>
-              <Navlink href="/resume">Resume</Navlink>
-              <Navlink href="/services">Services</Navlink>
-              <Navlink href="/contact">Contact</Navlink>
-              <Navlink href="/contact">Hire me</Navlink>
+              <Navlink onClick={handleClose} href="/">Home</Navlink>
+              <Navlink onClick={handleClose} href="/project">Project</Navlink>
+              <Navlink onClick={handleClose} href="/resume">Resume</Navlink>
+              <Navlink onClick={handleClose} href="/services">Services</Navlink>
+              <Navlink onClick={handleClose} href="/contact">Contact</Navlink>
+              <Navlink onClick={handleClose} href="/contact">Hire me</Navlink>
             </SheetContent>
           </Sheet>
         </nav>
--- src/components/Navlink.jsx
+++ src/components/Navlink.jsx
@@ -3,13 +3,13 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
-const Navlink = ({href, children}) => {
+const Navlink = ({href, children, onClick}) => {
     const pathname = usePathname()
 
     const isActive = pathname === href
 
   return (
-    <Link href={href} className={` ${isActive ? "active font-semibold" : ""} ${children === "Hire me" ? "bg-[#00e187] rounded-full hover:bg-[#00e191] text-center h-9 flex items-center px-3 py-4 !text-black " : "text-nav"} transitions duration-200`}>
+    <Link onClick={onClick} href={href} className={` ${isActive ? "active font-semibold" : ""} ${children === "Hire me" ? "bg-[#00e187] rounded-full hover:bg-[#00e191] text-center h-9 flex items-center px-3 py-4 !text-black " : "text-nav"} transitions duration-200`}>
       {children}
     </Link>
   )
